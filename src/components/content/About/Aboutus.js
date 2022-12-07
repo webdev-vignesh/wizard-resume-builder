86ea10ad6b13f7bb28e506ec2a079e737defc4d3
suriyaofficial <suriyaboss1@gmail.com>
2022-12-07 20:12:41 +0530

    about pages is chnaged

--- src/components/content/About/Aboutus.js
+++ src/components/content/About/Aboutus.js
@@ -4,9 +4,10 @@ import Navlogo from './Navlogo.jpg'
 function Aboutus() {
   return (
   <div className='about'>
-    <div className='left'>
+    <div>
       <h1>Project</h1>
       <h1>Resume Builder Website</h1>
+      <img src={Navlogo} alt=""/>
       <p>A web application using which you can create your professional work resume in a few minutes. </p>
 <p>We will provide you with a set of templates to choose from and then enter all your details. </p>
 <p>Then click 'Finish' </p>
@@ -14,10 +15,7 @@ function Aboutus() {
     <h1>Created by</h1>
     <h3>Web3 Wizards</h3>
     </div>
-    <div className='right'>
-      <img src={Navlogo} alt=""/>
-
-    </div>
+    
     </div>
   )
 }
