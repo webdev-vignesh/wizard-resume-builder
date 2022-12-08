3c88e71928ec829be1babebf63ff468cd40a28ca
vignesh murugan <vigmurug@gmail.com>
2022-12-08 14:44:35 +0530

    Updated Navbar component using Material UI code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
 import Navlogo from '../assets/navlogo.jpg'
-function Navbar() {
-  return (
+import '../styleSheets/NavBar.css';
+import {AppBar, Toolbar, Stack, Button, Typography} from '@mui/material';
 
-    <div className='Nav'>
-        <div>
-          <img  className='img' src={Navlogo}alt=""></img>
-        </div>
-        <div className='Nav-items'>
-          <Link className='Nav-link'to="home">Templete</Link>
-          <Link className='Nav-link'to="about">About Page</Link>
-        </div>
-    </div>
-    
-    
- 
-  )
+const NavBar = () => {
+    return(
+      <AppBar position='static' style={{ background: '#FFFFFF' }}>
+        <Toolbar>
+          <img src={Navlogo} alt='logo'/>
+          <Typography sx={{flexGrow: 1}}></Typography>
+          <Stack direction='row' spacing={2} style={{ color: '#111'}}>
+            <Button color="inherit">Resume Templates</Button>
+            <Button color="inherit">My Resume</Button>
+            <Button color="inherit">About Us</Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    )
 }
 
-export default Navbar
+export default NavBar;
